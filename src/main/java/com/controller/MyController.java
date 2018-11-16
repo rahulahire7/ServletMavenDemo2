9c75dea507db22aecb355346dfa7eca8eed35ab2
rahulahire7 <rahul.ahire@gmail.com>
2018-11-16 13:16:53 +0530

    new maven app

---
+++ src/main/java/com/controller/MyController.java
@@ -0,0 +1,43 @@
+package com.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyController
+ */
+public class MyController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter pw=response.getWriter();
+		String str=request.getParameter("uname");
+		pw.print("Welcome :"+str);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<html>
+<body>
+	<h2>Hello World!</h2>
+	<form action="MyController">
+		<input type="text" name="uname">
+		<input type="submit" value="click">
+		
+	</form>
+</body>
+</html>
